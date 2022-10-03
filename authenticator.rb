2d8fc89c328883612c7694b0778c29aa2511cb53
Alejandro.Carvajal <alejandro.carvajal@Work-Computer.local>
2022-10-03 15:25:01 -0600

    Add check function to compare user input to credentials.

---
+++ authenticator.rb
@@ -0,0 +1,29 @@
+users= [
+    {username: "alex" , password: "alex"},
+    {username: "ellie" , password: "ellie"}
+]
+
+puts "Welcome to the Authenticator"
+25.times {print "-"}
+puts "This program will take input from the user and compare to the credentials"
+puts "If authentication is successful, the user object will be displayed"
+
+p attempts= 1
+while attempts <4
+    print "Username: "
+    username= gets.chomp 
+    print "Password: "
+    password= gets.chomp
+    users.each do |user|
+        if user[:username]==username && user[:password]==password
+            puts user
+        else 
+            puts "Invalid Email/Passord"
+        end
+    end
+    puts "Press n to quit or any other key to continue:"
+    input= gets.chomp.downcase
+    break if input=="n"
+    p attempts+=1
+end
+
